86e7e383c5921ffec15bb5b945ded97011c1e45a
nam.t <namtran09061992@gmail.com>
2016-11-15 14:12:34 +0700

    Life cycle Activity

--- app/src/main/java/namtran/lifecycle/MainActivity.java
+++ app/src/main/java/namtran/lifecycle/MainActivity.java
@@ -1,31 +1,105 @@
 package namtran.lifecycle;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView txtLifeCycle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        txtLifeCycle = (TextView) findViewById(R.id.txtLifeCycle);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        if (savedInstanceState != null){
+            Log.d("LifeCyCle","onCreate  " + getClass().getSimpleName());
+            txtLifeCycle.append(savedInstanceState.getString("LifeCycle"));
+            txtLifeCycle.append("onCreate Activity" + "\n\n");
+        }else {
+            Log.d("LifeCyCle","onCreate  " + getClass().getSimpleName());
+            txtLifeCycle.append("onCreate Activity" + "\n\n");
+        }
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("LifeCyCle","onStart  " + getClass().getSimpleName());
+        txtLifeCycle.append("onStart Activity" + "\n\n");
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        Log.d("LifeCyCle","onPostCreate(@Nullable Bundle savedInstanceState)  " + getClass().getSimpleName());
+        txtLifeCycle.append("onPostCreate(@Nullable Bundle savedInstanceState) Activity" + "\n\n");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("LifeCyCle","onResume  " + getClass().getSimpleName());
+        txtLifeCycle.append("onResume Activity" + "\n\n");
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Log.d("LifeCyCle","onPostResume  " + getClass().getSimpleName());
+        txtLifeCycle.append("onPostResume Activity" + "\n\n");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("LifeCyCle","onPause  " + getClass().getSimpleName());
+        txtLifeCycle.append("onPause Activity" + "\n\n");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("LifeCyCle","onStop  " + getClass().getSimpleName());
+        txtLifeCycle.append("onStop Activity" + "\n\n");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("LifeCyCle","onRestart  " + getClass().getSimpleName());
+        txtLifeCycle.append("onRestart Activity" + "\n\n");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d("LifeCyCle","onSaveInstanceState(Bundle outState)  " + getClass().getSimpleName());
+        txtLifeCycle.append("onSaveInstanceState(Bundle outState) Activity" + "\n\n");
+        outState.putString("LifeCycle",txtLifeCycle.getText().toString());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.d("LifeCyCle","onRestoreInstanceState(Bundle savedInstanceState)  " + getClass().getSimpleName());
+        txtLifeCycle.append("onRestoreInstanceState(Bundle savedInstanceState) Activity" + "\n\n");
+    }
+    
+    private void initFragment(Fragment fragment){
+         
     }
 
     @Override
